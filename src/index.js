7d9f7ff206e3eb3bd91ed98ecc0651b7268023ad
Samala Sumanth <sumanth.samala@letsventure.com>
2020-03-12 15:52:06 +0530

    Fixed Import/export Issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import TextInput from '../src/Components/TextInput';
+import * as Yup from "yup";
+import CkEditor from '.../src/Components/CkEditor';
+import TextArea from '.../src/Components/TextArea';
+import RadioBox from '.../src/Components/RadioBox';
+import CheckBox from '.../src/Components/CheckBox';
+import PhotoUpload from '.../src/Components/PhotoUpload';
+import DropZone from '.../src/Components/DropZone';
+import DropDown from '.../src/Components/DropDown';
+import DatePicker from '.../src/Components/DatePicker';
+import TextGroup from '.../src/Components/TextGroup';
+
+export { TextInput, CkEditor, TextArea, RadioBox, CheckBox, PhotoUpload, DropZone, DropDown, DatePicker, TextGroup }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
